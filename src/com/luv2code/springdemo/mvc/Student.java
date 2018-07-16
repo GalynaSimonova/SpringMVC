aeff0bfccb9e53ba62f185e7a922e404a5ff0017
GalynaSimonova <simonov.galyna@gmail.com>
2018-07-16 20:22:18 +0300

    Text Fields

---
+++ src/com/luv2code/springdemo/mvc/Student.java
@@ -0,0 +1,39 @@
+package com.luv2code.springdemo.mvc;
+
+public class Student {
+	private String firstName;
+	private String lastName;
+	
+	public Student() {
+		
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
